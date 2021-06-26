3828fba2d07c082b2b2fd13e72b617e37f7e1a8a
kboeh <kboeh3@gmail.com>
2021-06-25 21:36:08 -0400

    exhibit-info page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
+import './App.css';
 import Footer from "./component/Footer/Footer";
 import "./component/Footer/Footer.css";
 import "./component/Footer/Footer.js";
 import Navbar from "./component/Navbar/Navbar";
 import "./component/Navbar/Navbar.css";
 import "./component/Navbar/Navbar.js";
+import ExhibitInfo from './component/ExhibitInfo/ExhibitInfo';
 
 function App() {
   return (
-    <Footer />
+    <div className="App">
+      <ExhibitInfo />
+      <Footer />
+    </div>
   );
 }
 
