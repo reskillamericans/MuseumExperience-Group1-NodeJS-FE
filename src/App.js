7917cb07d0278891ed1af9e05205749371c4c80b
kboeh <kboeh3@gmail.com>
2021-06-27 19:59:26 -0400

    homepage complete

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
-import "./App.css";
+import './App.css';
+import Homepage from './component/Homepage/Homepage';
 
 function App() {
   return (
     <div className="App">
-      {/* <ExhibitPage />
-      <Login />
-      <Navbar />
-      <Footer />
-      <ExhibitInfo /> */}
-      {/* <Account /> */}
+      <Homepage />
+
     </div>
   );
 }
