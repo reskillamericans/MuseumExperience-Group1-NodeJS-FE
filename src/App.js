7e762283a84292424f52e3864b80492605d38f77
Alvin Uchenna <43749581+Veri5ied@users.noreply.github.com>
2021-06-27 11:41:55 +0100

    package-lock json merge conflict

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
 /* import ExhibitPage from "./component/ExhibitPage/ExhibitPage"; */
+import Account from "./component/Account/Account";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Navbar />
       <Footer />
       <ExhibitInfo /> */}
+      <Account />
     </div>
   );
 }
--- src/component/Account/Account.js
+++ src/component/Account/Account.js
@@ -1,4 +1,3 @@
-import image48 from "./images/image 48.png";
 import image5 from "./images/image 5.png";
 import image10 from "./images/image 10.png";
 import image11 from "./images/image 10 (1).png";
@@ -11,68 +10,71 @@ import "./Account.css";
 
 function Account() {
   return (
-   <>
-   <body>
-    <header>
-    <div class="head">
-          <h1 class="my-account">MY ACCOUNT</h1>
-          <button class="search-button"><b>Search</b>
-          <img src={vector} alt="magnifier"/>
+    <>
+      <header>
+        <div className="head">
+          <h1 className="my-account">MY ACCOUNT</h1>
+          <button className="search-button">
+            <b>Search</b>
+            <img src={vector} alt="magnifier" />
           </button>
         </div>
-    </header>
-    <section class="main">
-         <div class="card">
-           <div class="card-header">
-           </div>
+      </header>
+      <section className="main">
+        <div className="card">
+          <div className="card-header"></div>
 
-           <div class="card-body"></div>
-              <div class="img-holder">
-                <img src={image36} alt="profilepic"/>
-              </div>
-
-          <div class="add-pic">
-          <img src={image47} alt="addpic"/>
+          <div className="card-body"></div>
+          <div className="img-holder">
+            <img src={image36} alt="profilepic" />
           </div>
 
-              <div class="text">
-              <h2 class="name">
-               John Smith
-              </h2>
-              <p>john.smith@email.com</p>
-              </div>
+          <div className="add-pic">
+            <img src={image47} alt="addpic" />
+          </div>
 
-              <div class="button1">
-                  <button class="add-exhibit"> Add Exhibit </button>
+          <div className="text">
+            <h2 className="name">John Smith</h2>
+            <p>john.smith@email.com</p>
+          </div>
+
+          <div className="button1">
+            <button className="add-exhibit"> Add Exhibit </button>
+          </div>
+
+          <div className="subscribed">
+            <div className="topbox-text">
+              <h2 className="exhibit-text1">Your Subscribed Exhibits</h2>
+              <h2 className="exhibit-text2">Upcoming Exhibits</h2>
+            </div>
+            <div className="topbox">
+              <div className="paintings">
+                <img src={image5} alt="pic1" />
+                <img src={image10} alt="pic2" />
+                <img src={image11} alt="pic3" />
               </div>
-    
-    
-    
-    <div class="subscribed">
-      <div class="topbox-text">
-      <h2 class="exhibit-text1">Your Subscribed Exhibits</h2>
-      <h2 class="exhibit-text2">Upcoming Exhibits</h2>
-      </div>
-      <div class="topbox">
-        <div class="paintings">
-      <img src={image5} alt="pic1"/>
-      <img src={image10} alt="pic2"/>
-      <img src={image11} alt="pic3"/>
-      </div>
-      </div>
-      <div class="bottombox">
-        <h2 class="q-history">Question History</h2>
-        <button class="firstname"><img src ={image49} class="check1" alt="checkbox"></img> Suspendisse luctus ornare iaculis.<img src ={plus} class="plus1" alt="plus"></img></button>
-        <button class="lastname"><img src ={image49} class="check2" alt="checkbox"></img>Donec ut feugiat leo. Nullam at mauris eros. <img src ={plus} class="plus2" alt="plus"></img></button>
-        <button class="email"><img src ={image49} class="check3" alt="checkbox"></img>Fusce ut consequat eros, eget vehicula ante <img src ={plus} class="plus3" alt="plus"></img></button>
+            </div>
+            <div className="bottombox">
+              <h2 className="q-history">Question History</h2>
+              <button className="firstname">
+                <img src={image49} className="check1" alt="checkbox" />{" "}
+                Suspendisse luctus ornare iaculis.
+                <img src={plus} className="plus1" alt="plus" />
+              </button>
+              <button className="lastname">
+                <img src={image49} className="check2" alt="checkbox" />
+                Donec ut feugiat leo. Nullam at mauris eros.{" "}
+                <img src={plus} className="plus2" alt="plus" />
+              </button>
+              <button className="email">
+                <img src={image49} className="check3" alt="checkbox" />
+                Fusce ut consequat eros, eget vehicula ante{" "}
+                <img src={plus} className="plus3" alt="plus" />
+              </button>
+            </div>
+          </div>
         </div>
-    </div>
-
-     
-
-     </div>    
-    </section>  
-    </body>          
+      </section>
     </>
   );
 }
