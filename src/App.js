2e680bdfe9c5f619ba0eca126541f0bf148cfec4
Talisa Foster <Talisanfoster@gmail.com>
2021-06-27 18:35:46 +0900

    created register page for main website

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
-import './App.css';
-import Footer from "./component/Footer/Footer";
+/*import './App.css';
+/*import Footer from "./component/Footer/Footer";
 import Navbar from "./component/Navbar/Navbar";
-import ExhibitInfo from './component/ExhibitInfo/ExhibitInfo';
-
+import ExhibitInfo from './component/ExhibitInfo/ExhibitInfo';*/
+import Register from './component/RegisterPage/register';
+import './component/RegisterPage/Register.css';
 function App() {
   return (
     <div className="App">
-      <ExhibitInfo />
-      <Footer />
+    
+      <Register/>
     </div>
   );
 }
