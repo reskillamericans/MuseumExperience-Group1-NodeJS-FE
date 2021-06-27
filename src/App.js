a9b2caeb6b3718e91cb3bb170a6582c5566d4175
Alvin Uchenna <43749581+Veri5ied@users.noreply.github.com>
2021-06-27 03:28:18 +0100

    file lost recovery

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import './App.css';
-import ExhibitPage from './component/ExhibitPage/ExhibitPage';
+import "./App.css";
+/* import ExhibitPage from "./component/ExhibitPage/ExhibitPage"; */
 
 function App() {
   return (
     <div className="App">
-      <ExhibitPage />
+      {/* <ExhibitPage />
+      <Login />
+      <Navbar />
+      <Footer />
+      <ExhibitInfo /> */}
     </div>
   );
 }
---
+++ src/component/Navbar/Navbar.js
@@ -0,0 +1,36 @@
+import logo from "./navbarimages/museumlogo.svg";
+import React from "react";
+import "./Navbar.css";
+
+function Navbar() {
+  return (
+    <header>
+      <div class="container">
+        <img src={logo} alt="Logo" class="logo" />
+
+        <p>Museum Experience</p>
+
+        <nav>
+          <ul>
+            <li>
+              <a href="#">Home</a>
+            </li>
+            <li>
+              <a href="#">Exhibits</a>
+            </li>
+            <li>
+              <a href="#">Register</a>
+            </li>
+            <li>
+              <a href="#">Login</a>
+            </li>
+            <li>
+              <a href="#">About Us</a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  );
+}
+export default Navbar;
